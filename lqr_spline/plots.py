0c1448aeca083af34f561e4a9cf0717d96430947
elianne-sacher <98421152+elianne-sacher@users.noreply.github.com>
2022-07-25 17:34:41 -0400

    Update plots.py

--- lqr_spline/plots.py
+++ lqr_spline/plots.py
@@ -1,20 +1,28 @@
+from hmac import trans_36
+from logging import logProcesses
 from re import M
-import numpy as np
 import yaml
-from matplotlib import pyplot as plt 
+
+import matplotlib.pyplot as plt
+import matplotlib.collections as mcol
+from matplotlib.legend_handler import HandlerLineCollection, HandlerTuple
+from matplotlib.lines import Line2D
+import numpy as np
+
+import time
 
 import lqr
 import sim_move
 import trajectory_shapes
 
 global I
-I = 20
+I = 5
 
 global dt
 dt = 1 / I # s
 
 global N
-N = 10
+N = 20
  # a large enough number
 
 with open('start.yaml') as f:
@@ -34,36 +42,77 @@ global pos_start, pos_end
 pos_start = (x_line_0, y_line_0)
 pos_end = (x_line_N, y_line_N)
 
+
+
 def main():
+    # check time differences between the DARE LQR and the CVXPY LQR
+    t_CVXPY = []
+    t_DARE = []
     # get the start location
-    s = [s_0]
-    loops = 5
+    S_cvxpy = [s_0]
+    S_dare = [s_0]
+    U_cvxpy = [np.array([[0],[0]])]
+    U_dare = [np.array([[0],[0]])]
     # Generate the discrete trajectory path (the reference)
-    s_refs, u_refs = trajectory_shapes.straight_line(pos_start, pos_end, N) 
-    # s_refs, u_refs = trajectory_shapes.circle((0.0, 0.0), 4.0, N, loops)
-    for i in range(N):
+    s_refs_og, u_refs_og = trajectory_shapes.straight_line(pos_start, pos_end, N) 
+    #s_refs_og, u_refs_og = trajectory_shapes.circle((0.0, 0.0), 4.0, N, 1)
+    # copy the refrences
+    s_refs = s_refs_og.copy()
+    u_refs = u_refs_og.copy()
+    for i in range(len(s_refs)-1):
         for j in range(I):
             # get the next control input vector from the lqr controller
-            u = lqr.lqr_controller(s[-1], s_refs[i], u_refs[i], dt)
+            t0 = time.perf_counter()
+            U_cvxpy.append(lqr.lqr_traj_track_cvxpy(S_cvxpy,s_refs,u_refs,dt)[0])
+            t1 = time.perf_counter()
+            U_dare.append(lqr.lqr_traj_track_dare(S_dare, s_refs, u_refs, dt)[0])
+            t2 = time.perf_counter()
+            t_CVXPY.append(t1-t0)
+            t_DARE.append(t2-t1)
             # get the system's state and control input matrices
             A = sim_move.getA()
-            B = sim_move.getB((s[-1].T)[0][2], dt)
-            # print("Theta " + str(i) + ":" + str((s[-1].T)[0][2]))
+            B_cvxpy = sim_move.getB(S_cvxpy[-1], dt)
+            B_dare = sim_move.getB(S_dare[-1], dt)
             # calculate the next step according to the state space model
-            s.append(sim_move.state_space_model(A, s[-1], B, u))
-    plt.title("coordinates") 
-    plt.xlabel("x") 
-    plt.ylabel("y")
-    (x, y) = unpack(s)
-    plt.plot(x, y, 'bo') 
-    (x_ref, y_ref) = unpack(s_refs)
-    plt.plot(x_ref, y_ref, 'go')
+            S_cvxpy.append(sim_move.state_space_model(A, S_cvxpy[-1], B_cvxpy, U_cvxpy[-1]))
+            S_dare.append(sim_move.state_space_model(A, S_dare[-1], B_dare, U_dare[-1]))
+        s_refs.pop(0)
+        u_refs.pop(0)
+    
+  
+    # get coordinates
+    (x_cvxpy, y_cvxpy) = unpack(S_cvxpy)
+    (x_dare, y_dare) = unpack(S_dare)
+    (x_ref, y_ref) = unpack(s_refs_og)
+    
+    # plot
+    fig, ax = plt.subplots()
+    l2, = ax.plot(x_cvxpy, y_cvxpy, '--og',markersize=10)
+    l3, = ax.plot(x_dare,y_dare, '--.b')
+    l1, = ax.plot(x_ref, y_ref, '--or')
+    
+    # make plot pretty
+    ax.legend((l1, l2, l3), ('Reference', 'State with CVXPY', 'State with DARE'), loc='upper right', shadow=True)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('Line')
+    
+    # plot
+    fig, ax1 = plt.subplots()
+    l4, = ax1.plot(np.array(range(1,len(t_CVXPY)+1)), np.array(t_CVXPY), 'r')
+    l5, = ax1.plot(np.array(range(1,len(t_DARE)+1)), np.array(t_DARE), 'g')
+    
+    # make plot pretty
+    ax1.legend((l4, l5), ('RUNTIME with CVXPY', 'RUNTIME with DARE'), loc='upper right', shadow=True)
+    ax1.set_xlabel('Run Count [1]')
+    ax1.set_ylabel('Run Time [s]')
+    ax1.set_title('Line')
     plt.show()
-
-def unpack(s):
+   
+def unpack(S):
     x = []
     y = []
-    for item in s:
+    for item in S:
         x.append(item.T[0][0])
         y.append(item.T[0][1])
     return(np.array(x), np.array(y))
