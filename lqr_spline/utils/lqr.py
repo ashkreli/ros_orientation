05932830d3043a9a7b7ed1dda6ca7e23e0ce6a4b
elianne-sacher <98421152+elianne-sacher@users.noreply.github.com>
2022-07-27 20:24:48 -0500

    Update lqr.py

--- lqr_spline/utils/lqr.py
+++ lqr_spline/utils/lqr.py
@@ -7,6 +7,7 @@ import sim_move
 
 def lqr_traj_track_cvxpy(S: List[np.array],s_refs: List[np.array],u_refs: List[np.array],dt: float) -> List[np.array]:
     """
+    This is the LQR Trajectory Tracking method.
     Produces the control input vector based on an LQR controller
     for the next s_refs and u_refs recieved as input
     This function utilizes CVXPY to solve for the controller.
@@ -61,6 +62,7 @@ def lqr_traj_track_cvxpy(S: List[np.array],s_refs: List[np.array],u_refs: List[n
 
 def lqr_traj_track_dare(S: List[np.array],s_refs: List[np.array],u_refs: List[np.array],dt: float) -> List[np.array]:
     """
+    This is the LQR Trajectory Tracking method.
     Produces the control input vector based on an LQR controller
     for the next s_refs and u_refs recieved as input.
     This function uses DARE to find the controller.
@@ -112,6 +114,116 @@ def lqr_traj_track_dare(S: List[np.array],s_refs: List[np.array],u_refs: List[np
         new_s.append(sim_move.state_space_model(sim_move.getA(), new_s[i], sim_move.getB(new_s[i],dt), u[i]))
     return u
 
+def lqr_evol_ref_cvxpy(S: List[np.array],s_refs: List[np.array],dt: float) -> List[np.array]:
+    """
+    This is the LQR Evolving Refrence Point method.
+    Produces the control input vector based on an LQR controller
+    for the next s_refs recieved as input
+    This function utilizes CVXPY to solve for the controller.
+    """
+
+    # R matrix - The control input cost matrix
+    R = np.array([[2,   0],  # Penalty for linear velocity error
+                  [0,   0.5]]) # Penalty for angular velocity error
+    # Q matrix - The state cost matrix.
+    Q = np.array([[2, 0, 0],  # Penalize X position error 
+                  [0, 2, 0],  # Penalize Y position error 
+                  [0, 0, 1]]) # Penalize ANGLE heading error  
+    
+    # setup the variables for the opt. prob.
+    u_list = []
+    s_list = []
+    for i in range(len(s_refs)):
+        s_list.append(cvx.Variable((3,1)))
+        u_list.append(cvx.Variable((2,1)))
+
+    # apply the constraints
+    constraints = [(s_list[0] == S[-1])]
+
+    # get the performance index
+    prf_inx = cvx.quad_form((s_list[0] - s_refs[0]),Q) + cvx.quad_form(u_list[0],R)
+    
+    # get the state matrix
+    A_hat = sim_move.getA()
+    # get the control input matrix
+    B_hat = sim_move.getB(S[-1],dt)
+
+    for t in range(1,len(s_refs)):
+    
+        # add the relavent constraint to the opt. question
+        constraints.append(s_list[t] == A_hat @ s_list[t-1] + B_hat @ u_list[t-1])
+
+        # calculate the performance index
+        prf_inx += cvx.quad_form((s_list[t] - s_refs[0]),Q) + cvx.quad_form((u_list[t]),R)
+
+    # get the objective of the opt. question
+    objective = cvx.Minimize(prf_inx)
+
+    # define the problem and solve it
+    prob = cvx.Problem(objective, constraints)
+    prob.solve()
+
+    # return the calculated control inputs 
+    U = []
+    for i in range(len(u_list)):
+        U.append(u_list[i].value)
+    return U
+
+def lqr_evol_ref_dare(S: List[np.array],s_refs: List[np.array], dt: float) -> List[np.array]:
+    """
+    This is the LQR Evolving Refrence Point method.
+    Produces the control input vector based on an LQR controller
+    for the next s_refs recieved as input.
+    This function uses DARE to find the controller.
+    """
+    
+    # R matrix - The control input cost matrix
+    R = np.array([[2,   0],  # Penalty for linear velocity error
+                  [0,   0.5]]) # Penalty for angular velocity error
+    # Q matrix - The state cost matrix.
+    Q = np.array([[2, 0, 0],  # Penalize X position error 
+                  [0, 2, 0],  # Penalize Y position error 
+                  [0, 0, 1]]) # Penalize ANGLE heading error  
+    
+    # Solutions to discrete LQR problems are obtained using the dynamic 
+    # programming method.
+    # The optimal solution is obtained recursively, starting at the last 
+    # timestep and working backwards.
+    N = len(s_refs) - 1
+ 
+    # Create a list of N + 1 elements
+    P = [None] * (N + 1)
+      
+    # LQR via Dynamic Programming
+    P[N] = Q
+ 
+    # get the state matrix
+    A_hat = sim_move.getA()
+    # get of control input matrix
+    B_hat = sim_move.getB(S[-1],dt)
+
+    # For i = N, ..., 1
+    for i in range(N, 0, -1):
+        
+        # Discrete-time Algebraic Riccati equation to calculate the optimal 
+        # state cost matrix
+        P[i-1] = Q + A_hat.T @ P[i] @ A_hat - (A_hat.T @ P[i] @ B_hat) @ np.linalg.pinv(
+            R + B_hat.T @ P[i] @ B_hat) @ (B_hat.T @ P[i] @ A_hat)      
+    
+    # Create a list of N elements
+    K = [None] * N
+    u = [None] * N
+    
+    # new S assuming no noise and open loop
+    new_s = [S[-1]]
+    # For i = 0, ..., N - 1
+    for i in range(N):
+        # Calculate the optimal feedback gain K
+        K[i] = np.linalg.pinv(R + B_hat.T @ P[i+1] @ B_hat) @ B_hat.T @ P[i+1] @ A_hat
+        u[i] = - K[i] @ (new_s[i] - s_refs[0])
+        new_s.append(sim_move.state_space_model(sim_move.getA(), new_s[i], sim_move.getB(new_s[i],dt), u[i]))
+    return u
+
 def getAtildes(s_refs: List[np.array], u_refs: List[np.array], dt: float) -> List[np.array]:
     """
     Calculates and returns the state matrices for the delta system
@@ -135,22 +247,3 @@ def getBtildes(s_refs: List[np.array], dt: float) -> List[np.array]:
                             [(dt * np.sin(s_ref[2]))[0], 0],
                             [0,                         dt]]))
     return B_tildes
-
-def getBhat(s: np.array, dt: float) -> np.array:
-    """
-    Calculates and returns the control input matrix for the point system
-    """
-    theta = (s.T)[0][2]
-    B =  np.array([[dt * (np.cos(theta)), 0],
-                   [dt * (np.sin(theta)), 0],
-                   [0,                   dt]])
-    return B
-
-def getAhat() -> np.array:
-    """
-    Calculates and returns the state matrix, A matrix
-    """
-    A = np.array([  [1.0, 0, 0],
-                    [0, 1.0, 0],
-                    [0, 0, 1.0]])
-    return A
